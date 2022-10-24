fa429390ea0403f6a5e193b69e3302a905ae417e
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2022-10-24 15:24:50 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 
-console.log(galleryItems);
+const imagePalette = document.querySelector(".gallery");
+const makeImage = ({preview, original, description}) => {  
+    return `
+      <div class="gallery__item">
+        <a class="gallery__link"  href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>
+    `
+  }
+
+const makeImagas = galleryItems
+  .map(makeImage)
+  .join('');
+
+imagePalette.insertAdjacentHTML('beforeend', makeImagas);
+
+imagePalette.addEventListener('click', onGalleryClick);
+function onGalleryClick(evt) {
+      evt.preventDefault();
+  }
+
+  const lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+    });
