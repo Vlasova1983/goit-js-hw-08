f538f20ba0d54a907973837d3f5b5e4681359d85
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2022-10-25 20:23:19 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+console.log(player);
+
+player.on ('timeupdate', function() {
+    console.log('played the video!');        
+}
+)
+
+// player.on ('timeupdate',_.throttle(()=> {
+//     console.log('played the video!');
+        
+// },300));
+
 
