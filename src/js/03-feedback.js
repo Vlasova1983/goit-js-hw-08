2e78d4552c8712cd7e561e0832c967ec2db4680e
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2022-10-27 14:41:38 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,21 @@
+const inputEmail = document.querySelector("input");
+const inputMassege = document.querySelector("textarea");
+const formContainer = {email:"",messege:""};
 
+
+inputEmail.addEventListener ('input', onInputInput);
+inputMassege.addEventListener ('input', onInputMassege);
+function onInputInput() {   
+    formContainer.email=inputEmail.value;
+   console.log(formContainer);
+
+}
+function onInputMassege() {   
+   
+    formContainer.messege = inputMassege.value;
+   console.log(formContainer);
+}
+
+const email = localStorage.setItem("feedback-form-stat", formContainer);
+
+ 
