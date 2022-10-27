5d81a3e92f16b22d4d3747cba1c0dee3ec7bd171
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2022-10-27 14:41:31 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,19 +2,16 @@ import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-
-
-player.on ('timeupdate',_.throttle( function() {
-    console.log('played the video with throttle!');
-     
+  
+player.on ('timeupdate',_.throttle(function(evt) { 
+    localStorage.setItem("videoplayer-current-time", evt.seconds);       
 },1000));
 
-// console.log(player.getCurrentTime());  
-
-player.setCurrentTime(100).then(function(seconds){})
-.catch(function(error) {
-    switch (error.name) {
-        case 'RangeError': break;
-        default: break;
+player.setCurrentTime(Number(localStorage.getItem("videoplayer-current-time"))).then(function(seconds){})
+    .catch(function(error) {
+        switch (error.name) {
+            case 'RangeError': break;
+            default: break;
+        }
     }
-});
+);
