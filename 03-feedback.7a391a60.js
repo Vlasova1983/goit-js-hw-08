2a39173486219102b1944539cf43bf5536a6fd39
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2022-10-27 15:23:53 +0000

    Deploying to gh-pages from @ Vlasova1983/goit-js-hw-08@10f5d5a52ade05cad3f0eb6e8ea67276bf280a85 🚀

---
+++ 03-feedback.7a391a60.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("input"),t=document.querySelector("textarea"),a=document.querySelector(".feedback-form"),o=[];a.addEventListener("input",(function(){o[0]=e.value,o[1]=t.value,localStorage.setItem("feedback-form-state",o)})),a.addEventListener("submit",(function(){localStorage.clear("feedback-form-state")})),console.log(localStorage.length),localStorage.getItem("feedback-form-state")&&(e.value=localStorage.getItem("feedback-form-state"),t.value=localStorage.getItem("feedback-form-state"))}();
+//# sourceMappingURL=03-feedback.7a391a60.js.map
