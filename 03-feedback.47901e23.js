9a291701a1f5ec83b59395da615d2a851460936f
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2022-10-28 07:43:15 +0000

    Deploying to gh-pages from @ Vlasova1983/goit-js-hw-08@38dc843d0b9a7d3148cc35fee4065f3cbedc73c9 🚀

--- 03-feedback.47901e23.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("input"),t=document.querySelector("textarea"),a=document.querySelector(".feedback-form"),o=[];a.addEventListener("input",(function(){o[0]=e.value,o[1]=t.value,localStorage.setItem("feedback-form-state",o)})),a.addEventListener("submit",(function(){localStorage.clear("feedback-form-state")})),console.log(localStorage.length),localStorage.getItem("feedback-form-state")&&(e.value=localStorage.getItem("feedback-form-state"),t.value=localStorage.getItem("feedback-form-state"));
-//# sourceMappingURL=03-feedback.47901e23.js.map
--- 03-feedback.7a391a60.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("input"),t=document.querySelector("textarea"),a=document.querySelector(".feedback-form"),o=[];a.addEventListener("input",(function(){o[0]=e.value,o[1]=t.value,localStorage.setItem("feedback-form-state",o)})),a.addEventListener("submit",(function(){localStorage.clear("feedback-form-state")})),console.log(localStorage.length),localStorage.getItem("feedback-form-state")&&(e.value=localStorage.getItem("feedback-form-state"),t.value=localStorage.getItem("feedback-form-state"))}();
-//# sourceMappingURL=03-feedback.7a391a60.js.map
